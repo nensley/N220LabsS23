56c4d9a47cb16285b8c87d75ced797148313469f
nensley <nathanensley11@gmail.com>
2023-01-12 11:41:34 -0500

    Creating Lab 0

---
+++ Labs/Lab0/js/app.js
@@ -0,0 +1,10 @@
+function setup() {
+    createCanvas(1200, 768);
+    background(244, 167, 252);
+    fill(156, 255, 240);
+    rect(40, 50, 100, 20);
+}
+
+function draw() {
+    circle(mouseX, mouseY, 20);
+}
