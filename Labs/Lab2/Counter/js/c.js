0ffd9b8fb174ddf530b6bf135b549fc71459ebf2
nensley <nathanensley11@gmail.com>
2023-01-20 14:01:46 -0500

    Added Lab 2

---
+++ Labs/Lab2/Counter/js/c.js
@@ -0,0 +1,5 @@
+//Sets a variable to 0
+
+//Increases the variable by 1 every frame
+
+//Logs the variable in the console
---
+++ Labs/Lab2/PuckSlide/js/p.js
@@ -0,0 +1,11 @@
+//Sets the Canvas Size to 400x300
+
+//Changes the Color Fill of the Circle to be a variable
+
+//Draws a circle at Mouse X and Y
+
+//If the x coordinate of the mouse location is greater than 200 pixels
+//Then, the variable determining the circle's color will be red
+
+//If the x coordinate of the mouse location is less than 200 pixels
+//Then, the variable determining the circle's color will be blue
