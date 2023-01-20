089840b869d1ebcb3406b1ec1c187b38a4d076a9
nensley <nathanensley11@gmail.com>
2023-01-20 16:01:21 -0500

    JS Update

---
+++ Labs/Lab2/WorldWrap/js/w.js
@@ -0,0 +1,11 @@
+//defines the starting location of the circle as the far left of the screen
+
+//Sets the Canvas Width to 800 pixels and Canvas Height to 600 pixels
+
+//Draws a circle with the x coordinate as a variable
+
+//If the x coordinate of the circle is less than 800 pixels
+//Then, move the circle to the right 5 pixels
+
+//If the x coordinate is greater than 800 pixels
+//Then, reset the x coordinate to the left side of the screen
