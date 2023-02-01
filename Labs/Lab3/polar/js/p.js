3c877de9d666ed746ff6cbdd3deb42e7d7fa6fb7
nensley <nathanensley11@gmail.com>
2023-02-01 16:17:31 -0500

    Algorithm Check

---
+++ Labs/Lab3/polar/js/p.js
@@ -0,0 +1,11 @@
+// Nathan Ensley
+// 02/02/23
+// N220
+
+//Set a variable to represent the polar point
+//Write a function where x and y variables are determined by the polar point times the sin of the mouse's x or y position
+//Use the x and y coordinates to create a vector
+
+//set a varible equal to calling the polar point function
+//Add a translate
+//Draw a circle at the x and y points of that variable with a radius of 10
---
+++ Labs/Lab3/redremover/js/r.js
@@ -0,0 +1,8 @@
+// Nathan Ensley
+// 02/02/23
+// N220
+
+//write a function that sets the red component of a color to zero
+
+//defines a variable to represent the function acting on a chosen color
+//draws a circle with the new color
