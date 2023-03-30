613aba701e34fbcfce929e2b04e173bffc662372
nensley <nathanensley11@gmail.com>
2023-03-30 15:26:04 -0400

    js annotation

--- Labs/Lab8/random/js/app.js
+++ Labs/Lab8/random/js/app.js
@@ -4,13 +4,13 @@
 
 //have a button that runs a function when pressed
 //a seperate function should run the math random function to generate a number between 0 and 1
-//multiply this random number by 11 to get a number between 1 and 10 when the number is rounded down
+//multiply this random number by 11 to get a number between 0 and 10 when the number is rounded down
 //add the random number to the page in a div
 
 //extracts the div from the html document
 output = document.getElementById("txt")
 
-//generates random number from 1 to 10
+//generates random number from 0 to 10
 function randomGen(){
     return (Math.floor(Math.random() * 11))
 }
